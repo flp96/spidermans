dd1844ac0c0d93231935777d217bd476317ff293
flp96 <filipefernandes__@live.com>
2025-01-06 20:55:31 -0300

    evento de clique adicionado aos cards

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,26 +10,32 @@ function handleMouseLeave() {
 
 function addEventListenersToCards() {
   const cardElements = document.getElementsByClassName("s-card");
-
+  
   for (let i = 0; i < cardElements.length; i++) {
     const card = cardElements[i];
     card.addEventListener("mouseenter", handleMouseEnter);
     card.addEventListener("mouseleave", handleMouseLeave);
+
+    card.addEventListener("click", () => selectCarouselItem(card));
+  }
+}
+
+function selectCarouselItem(selectedCard) {
+  const selectedItemIndex = Array.from(selectedCard.parentNode.children).indexOf(selectedCard);
+  const carousel = document.querySelector(".s-cards-carousel");
+
+  const rotateYDeg = -120 * selectedItemIndex;
+  carousel.style.transform = `translateZ(-40vw) rotateY(${rotateYDeg}deg)`;
+
+  const activeButtonElement = document.querySelector('.s-controller__button--active');
+  if (activeButtonElement) {
+    activeButtonElement.classList.remove('s-controller__button--active');
+  }
+
+  const controllerButtons = document.querySelectorAll('.s-controller__button');
+  if (controllerButtons[selectedItemIndex]) {
+    controllerButtons[selectedItemIndex].classList.add('s-controller__button--active');
   }
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
-
-function selectCarouselItem(selectedButtonElement) {
-  const selectedItem = selectedButtonElement.id;
-  const carousel = document.querySelector(".s-cards-carousel");
-  const transform = carousel.style.transform;
-  const rotateY = transform.match(/rotateY\((-?\d+deg)\)/i);
-  const rotateYDeg = -120 * (Number(selectedItem) - 1);
-  const newTransform = transform.replace(rotateY[0], `rotateY(${rotateYDeg}deg)`);
-  carousel.style.transform = newTransform;
-
-  const activeButtonElement = document.querySelector('.s-controller__button--active');
-  activeButtonElement.classList.remove('s-controller__button--active');
-  selectedButtonElement.classList.add('s-controller__button--active');
-}
